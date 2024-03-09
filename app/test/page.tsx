3f4de5ab4928aa965f3ed5bd5f10d677ecafc329
Farid Rahman <farid.rahman@grabtaxi.com>
2024-03-09 08:39:03 +0700

    add init

---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import DefaultLayout from '../components/defaultLayout/DefaultLayout'
+
+
+function page() {
+  return (
+    <DefaultLayout></DefaultLayout>
+  )
+}
+
+export default page
