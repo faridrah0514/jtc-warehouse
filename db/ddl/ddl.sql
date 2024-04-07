0c0aaa9521a8b3372f415267aa8848b21ab4fd76
Farid Rahman <farid.rahman@grabtaxi.com>
2024-04-08 01:16:04 +0700

    a

--- db/ddl/ddl.sql
+++ db/ddl/ddl.sql
@@ -1,74 +1,99 @@
 -- jtc_warehouse.cabang definition
-
-CREATE TABLE `cabang` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nama_perusahaan` varchar(100) DEFAULT NULL,
-  `alamat` varchar(255) DEFAULT NULL,
-  `kota` varchar(100) DEFAULT NULL,
-  `no_tlp` varchar(100) DEFAULT NULL,
-  `status` varchar(100) DEFAULT NULL,
-  `kwh_rp` float DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
-
--- jtc_warehouse.aset definition
-
-CREATE TABLE `aset` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_aset` varchar(100) DEFAULT NULL UNIQUE,
-  `tipe_aset` varchar(100) DEFAULT NULL,
-  `nama_aset` varchar(100) DEFAULT NULL,
-  `id_cabang` int NOT NULL,
-  `alamat` varchar(255) DEFAULT NULL,
-  `kota` varchar(100) DEFAULT NULL,
-  `status` varchar(100) DEFAULT NULL,
-  `folder_path` varchar(255) DEFAULT NULL,
-  `no_tlp` varchar(100) DEFAULT NULL,
-  `no_rek_air` varchar(100) DEFAULT NULL,
-  `no_rek_listrik` varchar(100) DEFAULT NULL,
-  `no_pbb` varchar(100) default null,
-  `tipe_sertifikat` varchar(50) default null,
-  `no_sertifikat` varchar(100) default null,
-  `tanggal_akhir_hgb` varchar(50) default null,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`id_cabang`) REFERENCES `cabang` (`id`)
-) ;
+CREATE TABLE
+  `cabang` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `nama_perusahaan` varchar(100) DEFAULT NULL,
+    `alamat` varchar(255) DEFAULT NULL,
+    `kota` varchar(100) DEFAULT NULL,
+    `no_tlp` varchar(100) DEFAULT NULL,
+    `status` varchar(100) DEFAULT NULL,
+    `kwh_rp` float DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  );
 
 -- jtc_warehouse.pelanggan definition
+CREATE TABLE
+  `pelanggan` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `nama` varchar(100) DEFAULT NULL,
+    `alamat` varchar(255) DEFAULT NULL,
+    `kota` varchar(100) DEFAULT NULL,
+    `no_tlp` varchar(100) DEFAULT NULL,
+    `contact_person` varchar(100) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  );
 
-CREATE TABLE `pelanggan` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nama` varchar(100) DEFAULT NULL,
-  `alamat` varchar(255) DEFAULT NULL,
-  `kota` varchar(100) DEFAULT NULL,
-  `no_tlp` varchar(100) DEFAULT NULL,
-  `contact_person` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+CREATE TABLE
+  `tipe_aset` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `tipe_aset` varchar(100) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  );
+
+-- jtc_warehouse.aset definition
+CREATE TABLE
+  `aset` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `id_aset` varchar(100) DEFAULT NULL UNIQUE,
+    `tipe_aset` varchar(100) DEFAULT NULL,
+    `nama_aset` varchar(100) DEFAULT NULL,
+    `id_cabang` int NOT NULL,
+    `alamat` varchar(255) DEFAULT NULL,
+    `kota` varchar(100) DEFAULT NULL,
+    `status` varchar(100) DEFAULT NULL,
+    `folder_path` varchar(255) DEFAULT NULL,
+    `no_tlp` varchar(100) DEFAULT NULL,
+    `no_rek_air` varchar(100) DEFAULT NULL,
+    `no_rek_listrik` varchar(100) DEFAULT NULL,
+    `no_pbb` varchar(100) default null,
+    `tipe_sertifikat` varchar(50) default null,
+    `no_sertifikat` varchar(100) default null,
+    `tanggal_akhir_hgb` varchar(50) default null,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`id_cabang`) REFERENCES `cabang` (`id`)
+  );
+
+-- jtc_warehouse.aset definition
+CREATE TABLE
+  `aset` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `id_aset` varchar(100) DEFAULT NULL,
+    `id_tipe_aset` int NOT NULL,
+    `nama_aset` varchar(100) DEFAULT NULL,
+    `id_cabang` int NOT NULL,
+    `alamat` varchar(255) DEFAULT NULL,
+    `kota` varchar(100) DEFAULT NULL,
+    `status` varchar(100) DEFAULT NULL,
+    `folder_path` varchar(255) DEFAULT NULL,
+    `no_tlp` varchar(100) DEFAULT NULL,
+    `no_rek_air` varchar(100) DEFAULT NULL,
+    `no_rek_listrik` varchar(100) DEFAULT NULL,
+    `no_pbb` varchar(100) DEFAULT NULL,
+    `tipe_sertifikat` varchar(50) DEFAULT NULL,
+    `no_sertifikat` varchar(100) DEFAULT NULL,
+    `tanggal_akhir_hgb` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`id_cabang`) REFERENCES `cabang` (`id`),
+    FOREIGN KEY (`id_tipe_aset`) REFERENCES `tipe_aset` (`id`)
+  );
 
 -- jtc_warehouse.transaksi_sewa definition
-
-CREATE TABLE `transaksi_sewa` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_cabang` int DEFAULT NULL,
-  `id_pelanggan` int DEFAULT NULL,
-  `id_aset` int DEFAULT NULL,
-  `start_date_sewa` text,
-  `end_date_sewa` text,
-  `harga` float DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_cabang` (`id_cabang`),
-  KEY `id_pelanggan` (`id_pelanggan`),
-  KEY `id_aset` (`id_aset`),
-  FOREIGN KEY (`id_cabang`) REFERENCES `cabang` (`id`),
-  FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id`),
-  FOREIGN KEY (`id_aset`) REFERENCES `aset` (`id`)
-) ;
+CREATE TABLE
+  `transaksi_sewa` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `id_cabang` int DEFAULT NULL,
+    `id_pelanggan` int DEFAULT NULL,
+    `id_aset` int DEFAULT NULL,
+    `start_date_sewa` text,
+    `end_date_sewa` text,
+    `harga` float DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `id_cabang` (`id_cabang`),
+    KEY `id_pelanggan` (`id_pelanggan`),
+    KEY `id_aset` (`id_aset`),
+    FOREIGN KEY (`id_cabang`) REFERENCES `cabang` (`id`),
+    FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id`),
+    FOREIGN KEY (`id_aset`) REFERENCES `aset` (`id`)
+  );
 
 -- jtc_warehouse.tipe_aset definition
-
-CREATE TABLE `tipe_aset` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `tipe_aset` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ;
